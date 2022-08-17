6ad2ef0213b360fcde08c92080b1e57b6649e035
eccomaggio <eccomaggio@gmail.com>
2022-08-18 00:44:22 +0800

    fixed tab1 results scrolling issue + duplicate offlist words

---
+++ gruvbox_colours.js
@@ -0,0 +1,51 @@
+// setup palette dictionary
+const palette : {
+dark0_hard  : "#1d2021",	// xterm: 234 / 29-32-33
+dark0       : "#282828",	// xterm: 235 / 40-40-40
+dark0_soft  : "#32302f",	// xterm: 236 / 50-48-47
+dark1       : "#3c3836",	// xterm: 237 / 60-56-54
+dark2       : "#504945",	// xterm: 239 / 80-73-69
+dark3       : "#665c54",	// xterm: 241 / 102-92-84
+dark4       : "#7c6f64",	// xterm: 243 / 124-111-100
+dark4_256   : "#7c6f64",	// xterm: 243 / 124-111-100
+
+gray_245    : "#928374",	// xterm: 245 / 146-131-116
+gray_244    : "#928374",	// xterm: 244 / 146-131-116
+
+light0_hard : "#f9f5d7",	// xterm: 230 / 249-245-215
+light0      : "#fbf1c7",	// xterm: 229 / 253-244-193
+light0_soft : "#f2e5bc",	// xterm: 228 / 242-229-188
+light1      : "#ebdbb2",	// xterm: 223 / 235-219-178
+light2      : "#d5c4a1",	// xterm: 250 / 213-196-161
+light3      : "#bdae93",	// xterm: 248 / 189-174-147
+light4      : "#a89984",	// xterm: 246 / 168-153-132
+light4_256  : "#a89984",	// xterm: 246 / 168-153-132
+
+bright_red     : "#fb4934",	// xterm: 167 / 251-73-52
+bright_green   : "#b8bb26",	// xterm: 142 / 184-187-38
+bright_yellow  : "#fabd2f",	// xterm: 214 / 250-189-47
+bright_blue    : "#83a598",	// xterm: 109 / 131-165-152
+bright_purple  : "#d3869b",	// xterm: 175 / 211-134-155
+bright_aqua    : "#8ec07c",	// xterm: 108 / 142-192-124
+bright_orange  : "#fe8019",	// xterm: 208 / 254-128-25
+
+neutral_red    : "#cc241d",	// xterm: 124 / 204-36-29
+neutral_green  : "#98971a",	// xterm: 106 / 152-151-26
+neutral_yellow : "#d79921",	// xterm: 172 / 215-153-33
+neutral_blue   : "#458588",	// xterm: 66 / 69-133-136
+neutral_purple : "#b16286",	// xterm: 132 / 177-98-134
+neutral_aqua   : "#689d6a",	// xterm: 72 / 104-157-106
+neutral_orange : "#d65d0e",	// xterm: 166 / 214-93-14
+
+faded_red      : "#9d0006",	// xterm: 88 / 157-0-6
+faded_green    : "#79740e",	// xterm: 100 / 121-116-14
+faded_yellow   : "#b57614",	// xterm: 136 / 181-118-20
+faded_blue     : "#076678",	// xterm: 24 / 7-102-120
+faded_purple   : "#8f3f71",	// xterm: 96 / 143-63-113
+faded_aqua     : "#427b58",	// xterm: 66 / 66-123-88
+faded_orange   : "#af3a03"	// xterm: 130 / 175-58-3
+};
+
+// for (const color in palette){
+  
+// }
